0369a1c9be45f7549a7209d4a6d99827e1553c56
Vinícius Miranda Corrêa <vinicius.correa@zoho.com>
2020-02-12 17:24:12 -0300

    Update main.cpp

--- ths1230/main.cpp
+++ ths1230/main.cpp
@@ -1,25 +1,29 @@
 #include <Arduino.h>
-#define   D0   8
-#define   PINS 8
+#define   D0   11
+#define   PINS 12
 
-uint32_t measure;
+uint16_t measure;
 
 void parallel_set_inputs(void) {
-  REG_WRITE(GPIO_ENABLE_W1TC_REG, 0xFF << D0);
+  REG_WRITE(GPIO_ENABLE_W1TC_REG, 0x1DFF << D0);
 }
-uint8_t parallel_read(void) {
+uint16_t parallel_read(void) {
   uint32_t input = REG_READ(GPIO_IN_REG);
-  return (input >> D0);
+  uint32_t value = input >> D0;
+  uint32_t buffer = value >> 10;
+  return (value - 512 * buffer);
 }
 void setup() {
   Serial.begin(115200);
-  Serial.println("-----------------");
-  for (int i = 0; i < PINS; i++) {
-    pinMode(D0 + i, INPUT);
+  for (int i = 0; i < PINS+1; i++) {
+    if (D0 + i != 20)
+      pinMode(D0 + i, INPUT);
   }
   parallel_set_inputs();
+  delay(1000);
 }
 void loop() {
   measure = parallel_read();
   Serial.println(measure);
+  delay(500);
 }
